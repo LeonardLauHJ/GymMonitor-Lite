0d88bcbed35afbc8be246e2fb17661411457f692
tuzicomet <leo336570@gmail.com>
2025-07-30 03:38:56 +1200

    Generated Kotlin Spring Boot project with PostgreSQL support
    - Basic project setup from Spring Initializr
    - Set up for Java 21
    - Kotlin + Spring Boot 3.5.4 configured
    - PostgreSQL dependency added
    - Gradle build files and folder structure created

---
+++ src/main/kotlin/com/leonardlau/gymmonitor/gymmonitor_lite/GymmonitorLiteApplication.kt
@@ -0,0 +1,11 @@
+package com.leonardlau.gymmonitor.gymmonitor_lite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GymmonitorLiteApplication
+
+fun main(args: Array<String>) {
+	runApplication<GymmonitorLiteApplication>(*args)
+}
