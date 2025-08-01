53535b05fe23640382c535f14613ce01affc0af0
tuzicomet <leo336570@gmail.com>
2025-08-01 23:55:19 +1200

    findby functions added to User and Club repositories

--- src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/repository/ClubRepository.kt
+++ src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/repository/ClubRepository.kt
@@ -3,4 +3,6 @@ package com.leonardlau.gymmonitor.gymmonitorlite.repository
 import com.leonardlau.gymmonitor.gymmonitorlite.entity.Club
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ClubRepository : JpaRepository<Club, Int>
+interface ClubRepository : JpaRepository<Club, Int> {
+    fun findByCode(code: String): Club?
+}
--- src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/repository/UserRepository.kt
+++ src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/repository/UserRepository.kt
@@ -3,4 +3,6 @@ package com.leonardlau.gymmonitor.gymmonitorlite.repository
 import com.leonardlau.gymmonitor.gymmonitorlite.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, Int>
+interface UserRepository : JpaRepository<User, Int> {
+    fun findByEmail(email: String): User?
+}
