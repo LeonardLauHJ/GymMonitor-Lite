070709393afa46c31e853e73568e6630910c85f9
tuzicomet <leo336570@gmail.com>
2025-08-05 18:27:38 +1200

    Created a query method to count the number of visits made by a member

--- src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/repository/VisitRepository.kt
+++ src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/repository/VisitRepository.kt
@@ -3,4 +3,12 @@ package com.leonardlau.gymmonitor.gymmonitorlite.repository
 import com.leonardlau.gymmonitor.gymmonitorlite.entity.Visit
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface VisitRepository : JpaRepository<Visit, Int>
+interface VisitRepository : JpaRepository<Visit, Int> {
+    /**
+     * Counts the total number of visits made by the given member.
+     *
+     * @param member The member whose visits are being counted.
+     * @return The count of visits.
+     */
+    fun countByMember(member: User): Long
+}
