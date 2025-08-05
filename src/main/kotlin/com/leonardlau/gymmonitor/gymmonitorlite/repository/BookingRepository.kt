7be2b6a7b015117893b7d30dda67590b7d364ce9
tuzicomet <leo336570@gmail.com>
2025-08-05 18:24:28 +1200

    Added a query method for finding bookings by user, status, and start time
    will be used for finding upcoming bookings

--- src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/repository/BookingRepository.kt
+++ src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/repository/BookingRepository.kt
@@ -3,4 +3,11 @@ package com.leonardlau.gymmonitor.gymmonitorlite.repository
 import com.leonardlau.gymmonitor.gymmonitorlite.entity.Booking
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface BookingRepository : JpaRepository<Booking, Int>
+interface BookingRepository : JpaRepository<Booking, Int> {
+    /**
+     * Returns the list of bookings made by the given user, with the given status, and starting after the given start time.
+     */
+    fun findByMemberAndStatusAndGymClass_StartTimeAfter(
+        member: User, status: String, startTime: LocalDateTime
+    ): List<Booking>
+}
