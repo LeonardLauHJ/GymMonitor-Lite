ca0444efc25c2f6dbbedb227d703dbed9932f705
tuzicomet <leo336570@gmail.com>
2025-08-07 19:45:24 +1200

    updated TimetableEntryDto with kdoc, currentCapacity changed to currentBookings
    variable name change made for better semantics

--- src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/dto/TimetableEntryDto.kt
+++ src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/dto/TimetableEntryDto.kt
@@ -2,11 +2,21 @@ package com.leonardlau.gymmonitor.gymmonitorlite.dto
 
 import java.time.LocalDateTime
 
+/**
+ * Represents a single gym class entry in the gym Class Timetable.
+ * 
+ * @property className Name of the gym class.
+ * @property locationName Name of the location where the class is held.
+ * @property startTime Starting time of the class.
+ * @property durationMinutes Duration of the class in minutes.
+ * @property currentBookings Number of current bookings for the class.
+ * @property maxCapacity Maximum number of allowed participants.
+ */
 data class TimetableEntryDto(
     val className: String,
     val locationName: String,
     val startTime: LocalDateTime,
     val durationMinutes: Long,
-    val currentCapacity: Int,
+    val currentBookings: Int,
     val maxCapacity: Int
 )
