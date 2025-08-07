647bb915beda3e566e9bb1d0360b12812dc8255c
tuzicomet <leo336570@gmail.com>
2025-08-07 19:38:28 +1200

    functions added to GymClassRepository for finding scheduled gym classes

--- src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/repository/GymClassRepository.kt
+++ src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/repository/GymClassRepository.kt
@@ -1,9 +1,32 @@
 package com.leonardlau.gymmonitor.gymmonitorlite.repository
 
+import java.time.LocalDateTime
 import com.leonardlau.gymmonitor.gymmonitorlite.entity.GymClass
 import org.springframework.data.jpa.repository.JpaRepository
 
 /**
  * JPA Repository interface for managing GymClass entities.
  */
-interface GymClassRepository : JpaRepository<GymClass, Int>
+interface GymClassRepository : JpaRepository<GymClass, Int> {
+    /**
+     * Find all gym classes scheduled at a specific club between the given start and end times.
+     *
+     * @param clubId The ID of the club to filter classes by.
+     * @param start The start timestamp of the time range.
+     * @param end The end timestamp of the time range.
+     * @return A list of GymClass objects that start within the given time range, ordered by start time ascending.
+     */
+    fun findByLocation_Club_IdAndStartTimeBetweenOrderByStartTimeAsc(
+        clubId: Int,
+        start: LocalDateTime,
+        end: LocalDateTime
+    ): List<GymClass>
+
+    /**
+     * Find all classes scheduled at a specific club, ordered by their start time in ascending order.
+     *
+     * @param clubId The ID of the club to filter classes by.
+     * @return A list of GymClass objects for the given club, ordered by start time ascending.
+     */
+    fun findByLocation_Club_IdOrderByStartTimeAsc(clubId: Int): List<GymClass>
+}
