fa55f2cf57506ebb6e2191ab7016d6486eb6831f
tuzicomet <leo336570@gmail.com>
2025-08-09 01:36:42 +1200

    Scheduling enabled on the application
    scheduling will be used for automatically billing users

--- src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/GymmonitorLiteApplication.kt
+++ src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/GymmonitorLiteApplication.kt
@@ -2,7 +2,9 @@ package com.leonardlau.gymmonitor.gymmonitorlite
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class GymmonitorLiteApplication
 
