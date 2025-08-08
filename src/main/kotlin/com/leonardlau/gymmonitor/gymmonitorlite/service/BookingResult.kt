3a7912940b3fcfe043cbf99cf382d0a109c6bb43
tuzicomet <leo336570@gmail.com>
2025-08-08 20:21:44 +1200

    BookingResult sealed class created
    This will be used to define the different results a booking attempt can have (eg success, class full, weekly limit reached etc)

---
+++ src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/service/BookingResult.kt
@@ -0,0 +1,24 @@
+package com.leonardlau.gymmonitor.gymmonitorlite.service
+
+/**
+ * Represents the possible results of a booking attempt.
+ */
+sealed class BookingResult {
+    // This is a sealed class, which lists all the possible outcomes of a function (in this case, BookingResult()).
+    // Only one of these results is returned at a time, and Kotlin needs all cases to be handled.
+
+    /** Booking was successful. */
+    object Success : BookingResult()
+
+    /** The user has already booked this class. */
+    object AlreadyBooked : BookingResult()
+
+    /** The class is already full. */
+    object Full : BookingResult()
+
+    /** The class with the given ID was not found. */
+    object NotFound : BookingResult()
+
+    /** The user has reached their weekly booking limit. */
+    object TooManyThisWeek : BookingResult()
+}
