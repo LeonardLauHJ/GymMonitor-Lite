698975fab53cf7f4b014d591243ebf98b1f209a9
tuzicomet <leo336570@gmail.com>
2025-08-14 01:21:05 +1200

    Initialised StaffController.kt
    will be used for staff-only endpoints

---
+++ src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/controller/StaffController.kt
@@ -0,0 +1,10 @@
+package com.leonardlau.gymmonitor.gymmonitorlite.controller
+
+/**
+ * Controller for endpoints accessible by authenticated staff users.
+ */
+@RestController
+@RequestMapping("/api/staff")
+class StaffController() {
+
+}
