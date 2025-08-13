b7e444a70c160ffbc0cda5abb501670175cf7b16
tuzicomet <leo336570@gmail.com>
2025-08-13 20:07:03 +1200

    Created a DTO for the membership details page

---
+++ src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/dto/MembershipDetailsDto.kt
@@ -0,0 +1,23 @@
+package com.leonardlau.gymmonitor.gymmonitorlite.dto
+
+import java.time.Instant
+import java.time.LocalDate
+
+/**
+ * Represents a member's current membership details.
+ *
+ * @property clubName Name of the club the user is a member of.
+ * @property dateJoined Date the member joined the club.
+ * @property totalVisits Total number of recorded visits.
+ * @property membershipPlanName Name of the membership plan (null if not on a plan).
+ * @property nextBillingDate Next date the membership will be billed (null if staff or no plan).
+ * @property amountDue Amount owed to the club in dollars.
+ */
+data class MembershipDetailsDto(
+    val clubName: String,
+    val dateJoined: Instant,
+    val totalVisits: Int,
+    val membershipPlanName: String?,
+    val nextBillingDate: LocalDate?,
+    val amountDue: String
+)
