fc41ed235c812afdfee685c25cbeebd37709d3dc
tuzicomet <leo336570@gmail.com>
2025-08-17 01:31:07 +1200

    DTO created for classes in a staff member's schedule
    This DTO is identical to TimetableEntryDto except without the staff member's name (as it would be redundant)

---
+++ src/main/kotlin/com/leonardlau/gymmonitor/gymmonitorlite/dto/StaffScheduleEntryDto.kt
@@ -0,0 +1,22 @@
+package com.leonardlau.gymmonitor.gymmonitorlite.dto
+
+import java.time.LocalDateTime
+
+/**
+ * Represents a single gym class entry in a staff member's schedule.
+ * 
+ * @property className Name of the gym class.
+ * @property locationName Name of the location where the class is held.
+ * @property startTime Starting time of the class.
+ * @property durationMinutes Duration of the class in minutes.
+ * @property currentBookings Number of current bookings for the class.
+ * @property maxCapacity Maximum number of allowed participants.
+ */
+data class StaffScheduleEntryDto(
+    val className: String,
+    val locationName: String,
+    val startTime: LocalDateTime,
+    val durationMinutes: Long,
+    val currentBookings: Int,
+    val maxCapacity: Int
+)
