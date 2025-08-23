6c58343977ba4525b794a3052696512da9752b21
tuzicomet <leo336570@gmail.com>
2025-08-24 07:02:36 +1200

    Added RetrofitClient and ApiService interface
    - Set up RetrofitClient to provide a single reusable instance for API calls
    - Created an empty ApiService interface to define endpoints later
    - Also set up Gson converter and base URL

---
+++ android/app/src/main/java/com/leonardlau/gymmonitor/gymmonitorliteapp/data/remote/ApiService.kt
@@ -0,0 +1,11 @@
+package com.leonardlau.gymmonitor.gymmonitorliteapp.data.remote
+
+/**
+ * Defines all API endpoints that the app can call.
+ *
+ * Each function here corresponds to one HTTP request,
+ * and Retrofit will automatically generate the implementation.
+ */
+interface ApiService {
+
+}
---
+++ android/app/src/main/java/com/leonardlau/gymmonitor/gymmonitorliteapp/data/remote/RetrofitClient.kt
@@ -0,0 +1,31 @@
+package com.leonardlau.gymmonitor.gymmonitorliteapp.data.remote
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * RetrofitClient - singleton object that sets up Retrofit for the app.
+ *
+ * Retrofit is the library for talking to the backend API.
+ * It handles sending HTTP requests (GET, POST, etc.) and converting JSON responses into
+ * Kotlin objects automatically. This is all done asynchronously.
+ *
+ * This object creates one instance of Retrofit and provides ApiService, which lets us
+ * call the API from anywhere in the app without creating multiple instances.
+ */
+
+object RetrofitClient {
+
+    // Base URL of the backend API
+    private const val BASE_URL = "http://localhost:8080/"
+
+    // Single instance used to make API calls throughout the app
+    // The endpoints which can be called are defined in the ApiService Interface
+    val apiService: ApiService by lazy { // lazy initialisation so it's only created once and reused
+        Retrofit.Builder()
+            .baseUrl(BASE_URL) // Tell Retrofit where our API is hosted
+            .addConverterFactory(GsonConverterFactory.create()) // Use Gson to handle JSON
+            .build() // Build the Retrofit instance
+            .create(ApiService::class.java) // Generate an implementation of ApiService
+    }
+}
