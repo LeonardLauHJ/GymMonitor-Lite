2fef6c3d4c4173f6e41cf87bf6414a4eaedd72f2
tuzicomet <leo336570@gmail.com>
2025-08-24 08:56:29 +1200

    Defined an endpoint for signup in apiservice

--- android/app/src/main/java/com/leonardlau/gymmonitor/gymmonitorliteapp/data/remote/ApiService.kt
+++ android/app/src/main/java/com/leonardlau/gymmonitor/gymmonitorliteapp/data/remote/ApiService.kt
@@ -1,5 +1,11 @@
 package com.leonardlau.gymmonitor.gymmonitorliteapp.data.remote
 
+import com.leonardlau.gymmonitor.gymmonitorliteapp.data.model.SignupRequest
+import com.leonardlau.gymmonitor.gymmonitorliteapp.data.model.SignupResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 /**
  * Defines all API endpoints that the app can call.
  *
@@ -7,5 +13,10 @@ package com.leonardlau.gymmonitor.gymmonitorliteapp.data.remote
  * and Retrofit will automatically generate the implementation.
  */
 interface ApiService {
-
+    /**
+     * Signup endpoint.
+     * @return Response<SignupResponse> to handle success and error separately
+     */
+    @POST("api/auth/signup")
+    suspend fun signup(@Body request: SignupRequest): Response<SignupResponse>
 }
