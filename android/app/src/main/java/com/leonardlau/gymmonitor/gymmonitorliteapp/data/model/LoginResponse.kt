80c28f8058c18aa96092a8234766ec0e00ba36c8
Leonard Lau <leo336570@gmail.com>
2025-08-26 02:28:06 +1200

    Fixed LoginResponse
    login endpoint should return the jwt token on success, not a message

--- android/app/src/main/java/com/leonardlau/gymmonitor/gymmonitorliteapp/data/model/LoginResponse.kt
+++ android/app/src/main/java/com/leonardlau/gymmonitor/gymmonitorliteapp/data/model/LoginResponse.kt
@@ -4,5 +4,5 @@ package com.leonardlau.gymmonitor.gymmonitorliteapp.data.model
  * Data class that represents the JSON response we get after a successful login.
  */
 data class LoginResponse(
-    val message: String
+    val token: String
 )
