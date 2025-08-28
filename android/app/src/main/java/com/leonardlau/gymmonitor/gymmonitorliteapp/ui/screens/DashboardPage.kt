90518926359a345883aac24b8b38bf11cb5e4432
Leonard Lau <leo336570@gmail.com>
2025-08-29 01:44:00 +1200

    Dashboard now displays a default profile picture
    styled to sit on the middle of the end of the header box.

--- android/app/src/main/java/com/leonardlau/gymmonitor/gymmonitorliteapp/ui/screens/DashboardPage.kt
+++ android/app/src/main/java/com/leonardlau/gymmonitor/gymmonitorliteapp/ui/screens/DashboardPage.kt
@@ -1,15 +1,21 @@
 package com.leonardlau.gymmonitor.gymmonitorliteapp.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.leonardlau.gymmonitor.gymmonitorliteapp.data.model.BookingSummary
@@ -17,6 +23,7 @@ import com.leonardlau.gymmonitor.gymmonitorliteapp.data.model.DashboardResponse
 import com.leonardlau.gymmonitor.gymmonitorliteapp.ui.components.DashboardStat
 import com.leonardlau.gymmonitor.gymmonitorliteapp.ui.components.ScreenTitle
 import java.time.format.DateTimeFormatter
+import com.leonardlau.gymmonitor.gymmonitorliteapp.R
 
 /**
  * UI composable for the member dashboard.
@@ -60,17 +67,35 @@ fun DashboardPage(
                     modifier = Modifier
                         .fillMaxSize()
                 ) {
-                    // Header section
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight(0.2f) // Cover the top 20% of the screen
-                            .background(color = Color(0xFF495D91)),
-                        contentAlignment = Alignment.Center // Center the title text
-                    ) {
-                        ScreenTitle(
-                            text = dashboard.dashboardTitle,
-                            color = Color.White // use white for contrast
+                    // Header section and Profile Picture
+                    Box {
+                        // Header section with title and background colour
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(180.dp)
+                                .background(color = Color(0xFF495D91)),
+                            contentAlignment = Alignment.Center // Center the title text
+                        ) {
+                            ScreenTitle(
+                                text = dashboard.dashboardTitle,
+                                color = Color.White,
+                                // Shift the title slightly up
+                                modifier = Modifier.offset(y = (-10).dp)
+                            )
+                        }
+
+                        // Profile picture, half overlapping the header
+                        Image(
+                            // Profile pictures not yet supported by backend, just use a default
+                            painter = painterResource(id = R.drawable.default_profile_icon),
+                            contentDescription = "Profile Picture",
+                            modifier = Modifier
+                                .size(130.dp)
+                                .align(Alignment.BottomCenter) // Bottom center of outer Box
+                                .offset(y = 65.dp) // Push down by half of the size to overlap the edge
+                                .clip(CircleShape), // Turn the image into a circle shape
+                            contentScale = ContentScale.Crop // Scale to fill container, crop overflow
                         )
                     }
 
@@ -78,7 +103,7 @@ fun DashboardPage(
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(top = 30.dp, bottom = 25.dp),
+                            .padding(top = 85.dp, bottom = 20.dp),
                         horizontalArrangement = Arrangement.SpaceEvenly
                     ) {
                         DashboardStat(
